9965f6dedcc8c33ff84cc0527ef03ef8635006df
craftidev <ivann.codev@gmail.com>
2024-10-23 02:56:09 +0200

    Games out of Player, own struct

--- internal/models.go
+++ internal/models.go
@@ -2,22 +2,25 @@ package internal
 
 // Main
 type Player struct {
+    ID      int
     Name   string
     FIDE   int
     Rating int
     Chance float64
+    Score int
     Standing int
-    Games []Game
 }
 
 type Game struct {
-    Number int
-    Opponent Player
-    PlayerResult *float64
+    ID int
+    PlayerOneID int
+    PlayerTwoID int
+    Winer int // -1, not played, 1 Player1, 2 Player2, 3 Draw
 }
 
 type GridData struct {
     Players []Player
+    Games []Game
     NumPrizes int
 }
 
